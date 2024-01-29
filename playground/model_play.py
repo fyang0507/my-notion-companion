6cf51f0ae025d44a2cb5db81f5e5072b11f5827f
Fred Yang <fredyang0507@gmail.com>
2024-01-29 16:02:42 -0500

    wrap llama into langchain chat model interfaces to enable using LCEL

--- playground/model_play.py
+++ playground/model_play.py
@@ -1,16 +1,37 @@
 from langchain_community.llms import LlamaCpp
+from langchain.prompts import PromptTemplate
+import json
 
 n_gpu_layers = 1  # Metal set to 1 is enough.
 n_batch = 512  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
 
 llm = LlamaCpp(
-    model_path='/Users/fred/Documents/models/baichuan2-7b-chat.Q4_K_S.gguf',
+    model_path='/Users/fred/Documents/models/chinese-alpaca-2-7b-q4_0.gguf',
     n_gpu_layers=n_gpu_layers,
     n_batch=n_batch,
     n_ctx=2048,
     f16_kv=True,
     verbose=True,
 )
+
+
+# prompt template from example script
+# https://github.com/ymcui/Chinese-LLaMA-Alpaca-2/blob/main/scripts/langchain/langchain_qa.py
+# official guide from HF: https://huggingface.co/blog/llama2#how-to-prompt-llama-2
+prompt_template = (
+    "[INST] <<SYS>>\n"
+    "You are a helpful assistant. 你是一个乐于助人的助手。\n"
+    "<</SYS>>\n\n"
+    "{context}\n{question} [/INST]"
+)
+
+prompt = PromptTemplate(
+    template=prompt_template, 
+    input_variables=["context", "question"]
+)
+
+chain = prompt | llm
+chain.invoke({'context': '', 'question': '你是谁？'})
 
 llm.invoke("who are you?")
 llm.invoke("你是谁？")
